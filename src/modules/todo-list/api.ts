47486d1e8e1b7246f1a8a9aa7b869cac650978ef
KozerT <kozer.vizastaff@gmail.com>
2025-03-02 21:58:19 +0100

    refactor: create an abstraction layer to make api call cleaner

--- src/modules/todo-list/api.ts
+++ src/modules/todo-list/api.ts
@@ -1,6 +1,5 @@
 import { infiniteQueryOptions, queryOptions } from "@tanstack/react-query";
-
-const BASE_URL = "http://localhost:3000";
+import { jsonApiInstance } from "../../shared/api/api-instance";
 
 export type PaginatedResult<T> = {
   data: T[];
@@ -17,20 +16,18 @@ export type TodoDto = {
   text: string;
   done: boolean;
 };
-export const TodoListApi = {
-  getTodoList: (
-    { page }: { page: number },
-    { signal }: { signal: AbortSignal },
-  ) => {
-    return fetch(`${BASE_URL}/tasks?_page=${page}&_per_page=4`, {
-      signal,
-    }).then((res) => res.json() as Promise<PaginatedResult<TodoDto>>);
-  },
 
+export const TodoListApi = {
   getTodoListQueryOptions: ({ page }: { page: number }) => {
     return queryOptions({
       queryKey: ["tasks, list", { page }],
-      queryFn: (meta) => TodoListApi.getTodoList({ page }, meta),
+      queryFn: (meta) =>
+        jsonApiInstance<PaginatedResult<TodoDto>>(
+          `/tasks?_page=${page}&_per_page=4`,
+          {
+            signal: meta.signal,
+          },
+        ),
     });
   },
 
@@ -38,8 +35,12 @@ export const TodoListApi = {
     return infiniteQueryOptions({
       queryKey: ["tasks, list"],
       queryFn: (meta) =>
-        TodoListApi.getTodoList({ page: meta.pageParam }, meta),
-
+        jsonApiInstance<PaginatedResult<TodoDto>>(
+          `/tasks?_page=${meta.pageParam}&_per_page=4`,
+          {
+            signal: meta.signal,
+          },
+        ),
       initialPageParam: 1,
       getNextPageParam: (result) => result.next,
       select: (result) => result.pages.flatMap((page) => page.data),
