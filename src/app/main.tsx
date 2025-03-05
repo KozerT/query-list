4edb8b092634613f5d4a7064e9ad1dc6e5c3871c
KozerT <kozer.vizastaff@gmail.com>
2025-03-05 14:53:44 +0100

    congif: install and configure dependencies for PWA support and offline mode

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,20 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../shared/api/query-client";
+import { createSyncStoragePersister } from "../../node_modules/@tanstack/query-sync-storage-persister/src/index";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 import { store } from "../shared/api/redux";
 import "./index.css";
 import { App } from "./app";
 
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+      onSuccess={() => {
+        // resume mutations after initial restore from localStorage was successful
+        queryClient.resumePausedMutations().then(() => {
+          queryClient.invalidateQueries();
+        });
+      }}
+    >
       <Provider store={store}>
         <App />
       </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>,
 );
